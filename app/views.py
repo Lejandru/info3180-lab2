ec549c76edf6edef871816f4a8d8494026f138f8
Lejandru Richards <Lejandru13@gmail.com>
2022-02-10 17:18:55 -0500

    Update

--- app/views.py
+++ app/views.py
@@ -7,7 +7,7 @@ This file creates your application.
 
 from app import app
 from flask import render_template, request, redirect, url_for, flash
-
+import datetime
 
 ###
 # Routing for your application.
@@ -22,12 +22,22 @@ def home():
 @app.route('/about/')
 def about():
     """Render the website's about page."""
-    return render_template('about.html', name="Mary Jane")
+    return render_template('about.html', name="Lejandru Richards")
+
+@app.route('/profile')
+def profile():
+    """Render the website's profile page."""
+    return render_template('profile.html', dateJo =format_date_joined())
+
 
 
 ###
 # The functions below should be applicable to all Flask apps.
 ###
+def format_date_joined():
+    i = datetime.datetime.now()
+    date_joined = datetime.date(2018, 1, 9)
+    return ("Joined " + date_joined.strftime("%B, %Y"))
 
 @app.route('/<file_name>.txt')
 def send_text_file(file_name):
